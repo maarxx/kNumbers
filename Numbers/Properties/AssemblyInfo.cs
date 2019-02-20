40a241b30fca8d6b639ea1a3c2d337c9226a0c16
Mehni <Mehni@users.noreply.github.com>
2019-02-20 13:19:23 +0100

    fix for overwritten rescue icon with different size

--- Numbers/Properties/AssemblyInfo.cs
+++ Numbers/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Numbers")]
-[assembly: AssemblyCopyright("Copyright © Mehni 2018")]
+[assembly: AssemblyCopyright("Copyright © Mehni 2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.3")]
