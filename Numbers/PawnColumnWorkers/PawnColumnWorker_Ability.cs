e4b637dbbe099fb8aa1edc5e7b2622d7d9898ac2
maarxx <chris.lewark@gmail.com>
2020-12-10 22:27:47 -0500

    Much better!

--- Numbers/PawnColumnWorkers/PawnColumnWorker_Ability.cs
+++ Numbers/PawnColumnWorkers/PawnColumnWorker_Ability.cs
@@ -4,52 +4,29 @@
     using UnityEngine;
     using Verse;
 
-    public class PawnColumnWorker_Ability : PawnColumnWorker_Checkbox
+    public class PawnColumnWorker_Ability : PawnColumnWorker_Icon
     {
-        public static float IconPositionVertical = 35f;
-        public static float IconPositionHorizontal = 5f;
 
-        protected override bool GetValue(Pawn pawn)
+        protected override Texture2D GetIconFor(Pawn pawn)
         {
             AbilityDef abilityDef = def.Ext().ability;
             foreach (Ability a in pawn.abilities.abilities)
             {
                 if (a.def == abilityDef)
                 {
-                    return true;
+                    return abilityDef.uiIcon;
                 }
             }
-            return false;
+            return null;
         }
 
-        protected override bool HasCheckbox(Pawn pawn) => pawn.HasPsylink;
-
-        protected override void SetValue(Pawn pawn, bool value)
+        public override int GetMinWidth(PawnTable table)
         {
-            // this space intentionally left blank
-        }
-
-        protected override string GetHeaderTip(PawnTable table) => "SelfTend".Translate() + "\n\n" + "Numbers_ColumnHeader_Tooltip".Translate();
-        //protected override string GetHeaderTip(PawnTable table) => def.Ext().ability.GetTooltip();
-        //protected override string GetHeaderTip(PawnTable table) => "";
-
-        protected override string GetTip(Pawn pawn)
-        {
-            if (GetValue(pawn))
-            {
-                return def.Ext().ability.GetTooltip();
-            }
-            return "";
+            return 26;
         }
 
         public override void DoHeader(Rect rect, PawnTable table)
         {
-            //base method would put text on the header because these defs have a label.
-            //we don't want text on the header, the icons are sufficient.
-            //but we need a bunch of crap from that method to make it interactable.
-            //so we avoid calling our parent and copy-paste their code here.
-
-            //base.DoHeader(rect, table);
             Rect interactableHeaderRect = GetInteractableHeaderRect(rect, table);
             if (Mouse.IsOver(interactableHeaderRect))
             {
@@ -65,12 +42,9 @@
                 HeaderClicked(rect, table);
             }
 
-            float scale = 0.5f;
             Texture2D abilityIcon = def.Ext().ability.uiIcon;
-            //Texture2D abilityIcon = ContentFinder<Texture2D>.Get(def.Ext().ability.iconPath);
-            Vector2 headerIconSize = new Vector2(abilityIcon.width, abilityIcon.height) * scale;
-            int     num            = (int)((rect.width - headerIconSize.x) / 2f);
-            Rect    position       = new Rect(rect.x + num + IconPositionHorizontal, rect.yMax - abilityIcon.height + IconPositionVertical, headerIconSize.x, headerIconSize.y);
+            Vector2 headerIconSize = new Vector2(26, 26);
+            Rect position = new Rect(rect.x, rect.yMax - 26, 26, 26);
             GUI.DrawTexture(position, abilityIcon);
         }
     }
